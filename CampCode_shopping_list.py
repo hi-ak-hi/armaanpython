c128ed54372b9b862f500d68ee575f84f1f490ea
hi-ak-hi <armaanpk002@gmail.com>
2024-07-20 17:38:55 -0400

    edidting code

--- CampCode_shopping_list.py
+++ CampCode_shopping_list.py
@@ -7,17 +7,17 @@ try:
         food=input("what food do you want:\n")
         check=input("do you want to check this item off;type yes or no:\n")
         if check=="yes" or check=="YES" or check=="Yes" or check=="yEs" or check=="yeS" or check=="YeS" or check=="YEs" or check=="yES":
-            SL.append("✔️"+food)
-            checked.append("✔️"+food)
+            SL.append("✔️  "+food)
+            checked.append("✔️  "+food)
         elif check=="no" or check=="NO" or check=="No" or check=="nO":
-            SL.append("⭕"+food)
+            SL.append("⭕  "+food)
             unchecked.append(food)
     for x in range(len(unchecked)):
-        check2=input("do you want to check the item " + unchecked[x])
+        check2=input("do you want to check the item " + unchecked[x] + ":\n")
         if check2 == "yes" or check2 == "YES" or check2 == "Yes" or check2 == "yEs" or check2 == "yeS" or check2 == "YeS" or check2 == "YEs" or check2 == "yES":
-            unchecked[x]="✔️"+unchecked[x]
+            unchecked[x]="✔️  "+unchecked[x]
         elif check2 == "no" or check2 == "NO" or check2 == "No" or check2 == "nO":
-            unchecked[x]="⭕" + unchecked[x]
+            unchecked[x]="⭕  " + unchecked[x]
     def printlist(list,list2):
         for x in range(len(list)):
             print(list[x])
