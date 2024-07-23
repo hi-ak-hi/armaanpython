1b9a5ee05dd9e77d1596e5cdf069d85803debbed
hi-ak-hi <armaanpk002@gmail.com>
2024-07-22 20:07:04 -0400

    CS50 stuff

--- CS50_exit.py
+++ CS50_exit.py
@@ -1,11 +1,8 @@
-guess = input("guess a number between 1 and 10. you have one chance")
-#one way of doing it
-'''if guess<=10 and guess>7 or guess>=0 and guess<7: 
-    print("sorry, you didnt guess it")
-else:
-    print("you got it")'''
-#beter way
-if int(guess)<=10 and int(guess)>7 or int(guess)>=0 and int(guess)<7: 
-    exit("sorry, you didnt guess it")
+import sys
 
-print("you got it")
+if len(sys.argv) < 2:
+    sys.exit("too few arguents")
+if len(sys.argv)>2:
+    sys.exit("to many arguents")
+
+print("my name is",sys.argv[1])
